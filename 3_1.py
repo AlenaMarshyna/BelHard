2fc0e3fa494c460ad3006cf6dc20574e1bf974d4
AlenaMarshyna <fitboxingbelarus@gmail.com>
2022-11-01 15:46:51 +0300

    add comments

--- 3_1.py
+++ 3_1.py
@@ -1,8 +1,10 @@
 #Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
 text = input('Enter your text:\n')
-text_new = text.replace(' ', '-')
-text_in_list = text.split()  #list without spaces
+
+text_new = text.replace(' ', '-')  #1 method
+
+text_in_list = text.split()  #2 method
 text_new2 = '-'.join(text_in_list)
 
 #resalt test
