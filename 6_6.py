bad370be683f21dc12bde5a19033f1706e67d4d3
Alena Marshyna <116918291+AlenaMarshyna@users.noreply.github.com>
2022-11-13 23:07:47 +0300

    Create 6_6.py
    Add function

---
+++ 6_6.py
@@ -0,0 +1,11 @@
+# Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
+ четные, потом нечётные
+
+def sort(numbers):
+    for i in range(len(numbers)):
+        if numbers[i] % 2:
+            numbers.append(numbers.pop(i))
+            
+    print(numbers)
+
+sort([3, 6, 8, 5, 10])  # проверка
