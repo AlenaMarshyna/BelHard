112e1263f8f359e497ec9aa79ff9ef9bef16aa12
AlenaMarshyna <fitboxingbelarus@gmail.com>
2022-11-15 22:22:12 +0300

    pep8

--- 7_1.py
+++ 7_1.py
@@ -11,13 +11,15 @@ while True:
     else:
         print('Going off the chessboard. Try again')
 
-def horse(x: int, y: int) -> tuple:
+
+def horse(x: int, y: int) -> list:
     step = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, -2], [-1, 2], [-2, 1], [-2, -1]]
     position = []
     chess = [1, 2, 3, 4, 5, 6, 7, 8]
     for i in step:
         if x + i[0] in chess and y + i[1] in chess:
-            position.append([x+i[0], y+i[1]])
+            position.append([x + i[0], y + i[1]])
     return position
+
 
 print(horse(x, y))
