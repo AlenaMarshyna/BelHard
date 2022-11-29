461c9e1198013e84e9f4ee818b3cad87bc297da1
AlenaMarshyna <fitboxingbelarus@gmail.com>
2022-11-29 11:37:23 +0300

    error correction

--- 10_1.py
+++ 10_1.py
@@ -10,13 +10,19 @@ from typing import Type
 
 class Car(object):
 
-    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool) -> None:
+    def __init__(
+            self,
+            color: str,
+            count_passenger_seats: int,
+            is_baby_seat: bool
+    ) -> None:
         self.color = color
         self.count_passenger_seats = count_passenger_seats
-        self.is_baby_seat: bool = False
+        self.is_baby_seat = is_baby_seat
+        self.is_busy = False
 
-    def __str__(self, is_baby_seat=False) -> str:
-        return f'Car: {self.color=}, {self.count_passenger_seats=}, {self.is_baby_seat=}'
+    def __str__(self) -> str:
+        return f'Car: {self.color=} {self.count_passenger_seats} {self.is_baby_seat=} {self.is_busy=}'
 
 
 """ Написать класс Taxi Конструктор класса принимает атрибуты:
@@ -36,18 +42,22 @@ class Taxi(object):
 
     def find_car(self, count_passengers: int, is_baby: bool) -> Car | None:
         for car in self.cars:
-            if not is_baby and car.count_passenger_seats >= count_passengers:
+            if not car.is_busy and car.count_passenger_seats >= count_passengers:
                 if is_baby and car.is_baby_seat:
-                    is_baby = True
+                    car.is_busy = True
                     return car
-                elif is_baby:
-                    return None
-                else:
-                    is_baby = True
+                elif not is_baby:
+                    car.is_busy = True
                     return car
 
-car1 = Car('red', 6, False)
-car2 = Car('red', 5, True)
-cars1 = [car1, car2]
-taxi = Taxi(cars=cars1)
-print(taxi.find_car(2, False))  # проверка
+
+car1 = Car(color='black', count_passenger_seats=5, is_baby_seat=True)
+car2 = Car(color='white', count_passenger_seats=4, is_baby_seat=False)
+car3 = Car(color='red', count_passenger_seats=5, is_baby_seat=False)
+cars = [car1, car2, car3]
+taxi = Taxi(cars=cars)
+print(taxi.find_car(5, True))
+print(taxi.find_car(4, False))
+print(taxi.find_car(4, False))
+print(taxi.find_car(4, False))
+
