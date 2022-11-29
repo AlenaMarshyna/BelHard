ada6a1f7a7a86d50a058c9b1a7651d358f21cea5
AlenaMarshyna <fitboxingbelarus@gmail.com>
2022-11-29 10:51:11 +0300

    error correction

--- 10_1.py
+++ 10_1.py
@@ -8,15 +8,15 @@ is_busy: bool (определяется внутри конструктора с
 from typing import Type
 
 
-class Car:
+class Car(object):
 
     def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool) -> None:
         self.color = color
         self.count_passenger_seats = count_passenger_seats
-        self.is_baby_seat = is_baby_seat
+        self.is_baby_seat: bool = False
 
     def __str__(self, is_busy=False) -> str:
-        return f'Car: {self.color}, {self.count_passenger_seats}, {self.is_baby_seat}'
+        return f'Car: {self.color=}, {self.count_passenger_seats=}, {self.is_baby_seat=}'
 
 
 """ Написать класс Taxi Конструктор класса принимает атрибуты:
@@ -29,20 +29,22 @@ cars: list[Car] (список экземпляров класса Car)
 """
 
 
-class Taxi():
+class Taxi(object):
 
     def __init__(self, cars: list[Car]) -> None:
         self.cars = cars
-        self.count_passengers = None
-        self.is_busy = None
 
-    def find_car(self, count_passengers: int, is_busy: bool) -> list:
-        self.count_passengers = count_passengers
-        self.is_busy = is_busy
-        if self.is_busy == False and self.count_passengers <= int(self.cars[1]):
-            return self.cars
-        else:
-            return None
+    def find_car(self, count_passengers: int, is_baby: bool) -> Car | None:
+        for car in self.cars:
+            if not car.is_baby and car.count_passenger_seats >= count_passengers:
+                if is_baby and car.is_baby_seat:
+                    car.is_baby = True
+                    return car
+                elif is_baby:
+                    return None
+                else:
+                    car.is_baby = True
+                    return car
 
 
 print(Taxi(['red', 6, False]).find_car(7, False))  # проверка
