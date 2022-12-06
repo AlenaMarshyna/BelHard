d3c384393c8b10d0e70b6be4536830c285e669d1
AlenaMarshyna <fitboxingbelarus@gmail.com>
2022-12-06 21:27:07 +0300

    new table

--- les11.py
+++ les11.py
@@ -1,73 +1,63 @@
 import sqlite3
 
-conn = sqlite3.connect('db.sqlite3')
+conn = sqlite3.connect('db11.sqlite3')
 cur = conn.cursor()
-
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS order_items(
-    id INTEGER NOT NULL PRIMARY KEY,
-    order_id INTEGER,
-    product_id INTEGER    
-    );
-''')
-conn.commit()
-
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS products(
-    title VARCHAR(36),
-    description VARCHAR(140),
-    category_id INTEGER,
-    id INTEGER NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES order_items(id)
-    ON DELETE CASCADE
-    );
-''')
-conn.commit()
-
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS categories(
-    name VARCHAR(24) UNIQUE,
-    id INTEGER NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES products(id)
-    ON DELETE CASCADE
-    );
-''')
-conn.commit()
-
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS orders(
-    user_id INTEGER,
-    status_id INTEGER,
-    id INTEGER NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES order_items(id)
-    ON DELETE CASCADE
-    );
-''')
-conn.commit()
-
 
 cur.execute('''
     CREATE TABLE IF NOT EXISTS statuses(
-    name VARCHAR(10) UNIQUE,
-    id INTEGER NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES orders(id)
-    ON DELETE CASCADE
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(10) UNIQUE
     );
 ''')
 conn.commit()
-
 
 cur.execute('''
     CREATE TABLE IF NOT EXISTS users(
-    name VARCHAR(24),
-    email VARCHAR(24) UNIQUE,
-    id INTEGER NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES orders(id)
-    ON DELETE CASCADE
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(24),
+        email VARCHAR(24) UNIQUE
     );
 ''')
 conn.commit()
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS orders(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    status_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE
+    );
+''')
+conn.commit()
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS categories(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(24) UNIQUE
+    );
+''')
+conn.commit()
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(36),
+    description VARCHAR(140),
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+    );
+''')
+conn.commit()
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS order_items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id INTEGER,
+        product_id INTEGER,
+        FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+        FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+    );
+''')
+conn.commit()
+
--- main.py
+++ main.py
@@ -215,3 +215,33 @@
 #
 # products = list(map(lambda x: Product(**x), products))
 # print(products)
+
+# import sqlite3
+#
+# conn = sqlite3.connect('db.sqlite3')
+# cur = conn.cursor()
+#
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS categories(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name VARCHAR(24) NOT NULL UNIQUE,
+#         is_published BOOLEAN DEFAULT(false)
+#     );
+# ''')
+# conn.commit()
+#
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS products(
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name VARCHAR(24) NOT NULL,
+#         descr VARCHAR(1024),
+#         price DECIMAL(7, 2) NOT NULL DEFAULT(0.0),
+#         category_id INTEGER NOT NULL,
+#         FOREIGN KEY (category_id) REFERENCES categories(id)
+#     );
+# ''')
+# conn.commit()
+#
+# cur.execute('''
+#
+# ''')
