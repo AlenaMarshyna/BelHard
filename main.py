028e9d08a6ce2435aff9391cfe622b195dfd01f5
Alena Marshyna <116918291+AlenaMarshyna@users.noreply.github.com>
2022-12-19 23:03:10 +0300

    Update main.py
    New

--- main.py
+++ main.py
@@ -1,3 +1,83 @@
+from sqlalchemy import Column, INT, VARCHAR, ForeignKey, \
+    DECIMAL, BOOLEAN, create_engine, select, BIGINT
+from sqlalchemy.orm import declarative_base, sessionmaker, declarative_mixin, Session
+
+Base = declarative_base()
+
+
+@declarative_mixin
+class BaseMixin(object):
+    id = Column(INT, primary_key=True)
+
+    engine = create_engine('postgresql://belhard:belhard@0.0.0.0:5432/bh35d')
+    _Session = sessionmaker(bind=engine)
+
+    @staticmethod
+    def create_session(func):
+        def wrapper(*args, **kwargs):
+            with BaseMixin._Session() as session:
+                return func(*args, **kwargs, session=session)
+
+        return wrapper
+
+    @create_session
+    def save(self, session: Session = None) -> None:
+        session.add(self)
+        session.commit()
+        session.refresh(self)
+
+    @classmethod
+    @create_session
+    def get(cls, pk, session: Session = None) -> Base | None:
+        return session.get(cls, pk)
+
+    @classmethod
+    @create_session
+    def all(cls, order_by: str = 'id', session: Session = None, **kwargs) -> list[Base]:
+        return session.scalars(
+            select(cls)
+            .filter_by(**kwargs)
+            .order_by(order_by)
+        ).all()
+
+    @create_session
+    def delete(self, session: Session = None):
+        session.delete(self)
+        session.commit()
+
+
+class Category(BaseMixin, Base):
+    __tablename__ = 'categories'
+
+    name = Column(VARCHAR(64), nullable=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseMixin, Base):
+    __tablename__ = 'products'
+
+    name = Column(VARCHAR(64), nullable=False)
+    price = Column(DECIMAL(8, 2), default=1)
+    is_published = Column(BOOLEAN, default=False)
+    category_id = Column(INT, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class User(BaseMixin, Base):
+    __tablename__ = 'users'
+
+    id = Column(BIGINT, primary_key=True)
+    email = Column(VARCHAR(32), nullable=False, unique=True)
+    name = Column(VARCHAR(32), nullable=False)
+
+    def __str__(self):
+        return self.email
+
+
 # def do_something(a, b=6, c=None):
 #     if not c:
 #         print(a*b)
